d620e739b35d950fe0fe1458f719667236a68508
Ryan Alyn Porter <rap@endymion.com>
2011-10-20 11:14:59 -0400

    More fine-tuning of the URLs.  Removed a Bakedweb URL and some example API information that didn't belong.  The name of the widget should include the name of the noun that it presents, so added 'Events' to the widget name.

--- public/js/AccountEventsWidget.js
+++ public/js/AccountEventsWidget.js
@@ -1,11 +1,9 @@
-var VenueDriverAccountWidget;
+var VenueDriverAccountEventsWidget;
 (function() {
-  VenueDriverAccountWidget = (function() {
-    function VenueDriverAccountWidget() {}
+  VenueDriverAccountEventsWidget = (function() {
+    function VenueDriverAccountEventsWidget() {}
 
   	options = {
-  		api_token: "NVJEZ3T8A861Q2",
-      account: "93",  
       div_id: "body",
       autostart: true
   	}
@@ -24,36 +22,36 @@ var VenueDriverAccountWidget;
   	jQuery.extend(options, params);
   	
     $(function initialize(){
-      VenueDriverAccountWidget.api_token = options.api_token;
-      VenueDriverAccountWidget.account = options.account;
-      VenueDriverAccountWidget.host = "venuedriver.com";
+      VenueDriverAccountEventsWidget.api_token = options.api_token;
+      VenueDriverAccountEventsWidget.account = options.account;
+      VenueDriverAccountEventsWidget.host = "venuedriver.com";
       options.div_id = "#" + options.div_id
       options.div_id = options.div_id.replace("#body", "body");
-      VenueDriverAccountWidget.api_url = "http://" + VenueDriverAccountWidget.host + "/api/accounts/" + VenueDriverAccountWidget.account + "/all_events?token=" + VenueDriverAccountWidget.api_token + "&callback=?";
-      VenueDriverAccountWidget.prepare_table();
+      VenueDriverAccountEventsWidget.api_url = "http://" + VenueDriverAccountEventsWidget.host + "/api/accounts/" + VenueDriverAccountEventsWidget.account + "/all_events?token=" + VenueDriverAccountEventsWidget.api_token + "&callback=?";
+      VenueDriverAccountEventsWidget.prepare_table();
     });
     
-    VenueDriverAccountWidget.prototype.start = function(){
+    VenueDriverAccountEventsWidget.prototype.start = function(){
       if(options.autostart == true){
-        $.getJSON(VenueDriverAccountWidget.api_url, function(data) { 
-          VenueDriverAccountWidget.prototype.fill_table(data);
+        $.getJSON(VenueDriverAccountEventsWidget.api_url, function(data) { 
+          VenueDriverAccountEventsWidget.prototype.fill_table(data);
         });
       }
     };
     
-    VenueDriverAccountWidget.prototype.fill_table = function(data) {
+    VenueDriverAccountEventsWidget.prototype.fill_table = function(data) {
       $.each(data, function(i, item) {
         $.each(item.events, function(i, event){
           var table_data = "<tr class='eventitem_row'><td width='92' class='eventitem_text column1'>" + event.date + "</td>  <td width='136' class='eventitem_text column2'>" + item.venue.city + ", " + item.venue.state +"</td>  <td width='130' class='eventitem_text column3'>" + item.venue.title + "</td>  <td class='eventitem_text column4'>" + event.title + "</td>  <td class='column5' align='right'><a href='" + urlBuyBtton(event, item)  + "' target='_blank'>Buy</a></td></tr>";
-          $('#venueWidgetMainTable tbody').append(table_data);
+          $('#venueDriverAccountEventsWidgetMainTable tbody').append(table_data);
         });
       });
       toggle_loading();
     };
     
-    VenueDriverAccountWidget.prepare_table = function(){
-      $(options.div_id).append("<div id='loading'><img src='http://github.com/Bakedweb/venue_widget/raw/master/public/images/ajax-loader.gif'/></div>")
-      $(options.div_id).append("<table id='venueWidgetMainTable'><thead><tr></tr></thead><tbody></tbody></table>");
+    VenueDriverAccountEventsWidget.prepare_table = function(){
+      $(options.div_id).append("<div id='loading'><img src='http://widgets.venuedriver.com/images/ajax-loader.gif'/></div>")
+      $(options.div_id).append("<table id='venueDriverAccountEventsWidgetMainTable'><thead><tr></tr></thead><tbody></tbody></table>");
       $(options.div_id + " tr").append(
               "<th class='eventlist_columnlabels' width='10%'>Date</th>" +
               "<th class='eventlist_columnlabels' width='20%'>Location</th>" +
@@ -74,14 +72,13 @@ var VenueDriverAccountWidget;
         return event.tickets_URL;
       }
     }
-  
     
-    return VenueDriverAccountWidget;
+    return VenueDriverAccountEventsWidget;
   })();
   $(function() {
     try{
-      var accountWidget = new VenueDriverAccountWidget;
-      return accountWidget.start();
+      var accountEventsWidget = new VenueDriverAccountEventsWidget;
+      return accountEventsWidget.start();
     }catch(e){
       console.log(e);
     }
