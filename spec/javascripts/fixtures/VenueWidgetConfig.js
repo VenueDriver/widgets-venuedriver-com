6a4b50d6a127c56246df7b266d04e43326718639
Rich Steinschneider <rcsteins@gmail.com>
2012-02-15 17:00:10 -0500

    lets make the fixtures more readable

--- spec/javascripts/fixtures/VenueWidgetConfig.js
+++ spec/javascripts/fixtures/VenueWidgetConfig.js
@@ -1,4 +1,27 @@
 VenueDriverWidgetConfig = new Object();
 VenueDriverWidgetConfig.autostart = false;
 VenueDriverWidgetConfig.account = '93';
-VenueDriverWidgetConfig.testData = [{"venue": {"city": "CityTest", "title": "TestVenue", "state": "T"}, "events": [{"id": "1","open": "2000/01/01 10:00:00 +0000", "title": "TestEvent", "date": "2011/01/01", "canceled": false, "tickets_URL": "http://localhost/TestURL", "description": "Don\u2019t miss Grammy nominated Producer \u0026 DJ, Morgan Page. His live sets merge the best in European Progressive House alongside his original hits and remixes; all tied together with a healthy dose of melody and ferocious beats. Download a live mix now at www.soundcloud.com/morganpage"},{"id": "2", "open": "2000/01/01 10:00:00 +0000", "title": "TestEvent2", "date": "2011/01/02", "canceled": false, "tickets_URL": null, "description": "Don\u2019t miss Grammy nominated Producer \u0026 DJ, Morgan Page. His live sets merge the best in European Progressive House alongside his original hits and remixes; all tied together with a healthy dose of melody and ferocious beats. Download a live mix now at www.soundcloud.com/morganpage"}], "friendly_id": "frindly_test_name"}]
+VenueDriverWidgetConfig.testData = 
+[{
+  "venue": {
+    "city": "CityTest", 
+    "title": "TestVenue", 
+    "state": "T"}, 
+  "events": [
+    {"id": "1",
+    "open": "2000/01/01 10:00:00 +0000",   
+    "title": "TestEvent", 
+    "date": "2011/01/01", 
+    "canceled": false, 
+    "tickets_URL": "http://localhost/TestURL", 
+    "description": "Don\u2019t miss Grammy nominated Producer \u0026 DJ, Morgan Page. His live sets merge the best in European Progressive House alongside his original hits and remixes; all tied together with a healthy dose of melody and ferocious beats. Download a live mix now at www.soundcloud.com/morganpage"},
+    {"id": "2", 
+    "open": "2000/01/01 10:00:00 +0000", 
+    "title": "TestEvent2", 
+    "date": "2011/01/02", 
+    "canceled": false, 
+    "tickets_URL": null, 
+    "description": "Don\u2019t miss Grammy nominated Producer \u0026 DJ, Morgan Page. His live sets merge the best in European Progressive House alongside his original hits and remixes; all tied together with a healthy dose of melody and ferocious beats. Download a live mix now at www.soundcloud.com/morganpage"
+  }], 
+  "friendly_id": "frindly_test_name"
+}]
