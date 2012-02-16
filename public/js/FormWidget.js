bb412c9b32c7cb1a8edb64e8dc743400ad3df214
Kevin Cummings <kevin.cummings@gmail.com>
2012-02-15 20:35:06 -0600

    updating FormWidget.js with basic plugin shell and updating demo.html with example basic usage

--- public/js/FormWidget.js
+++ public/js/FormWidget.js
@@ -1,33 +1,34 @@
-// var VenueDriverGuestListFormWidget;
-// (function() {
-//   VenueDriverGuestListFormWidget = (function() {
-//     function VenueDriverGuestListFormWidget() {}
-//     
-//     $(function initialize(){
-//     });
-//     
-//     VenueDriverGuestListFormWidget.prototype.render_data = function(data) {
-//       // Render API data into prepared div.
-//       toggle_loading();
-//     };
-//     
-//     VenueDriverGuestListFormWidget.prepare_div = function(){
-//       $(options.div_id).append("<div id='loading'><img src='http://widgets.venuedriver.com/images/ajax-loader.gif'/></div>")
-//       // Draw the form and a loading indicator so that users see something during the API call.
-//     }
-//     
-//     function toggle_loading(){
-//       $("#loading").toggle();
-//     }
-//     
-//     return VenueDriverGuestListFormWidget;
-//   })();
-//   $(function() {
-//     try{
-//       var guestListFormWidget = new VenueDriverGuestListFormWidget;
-//       return guestListFormWidget.start();
-//     }catch(e){
-//       console.log(e);
-//     }
-//   });
-// }).call(this);
+///////////////////////////////////////////////////////////////////////////////
+// Currently the plugin can be tested like:
+// $('#divID').VenueDriverFormWidget();
+///////////////////////////////////////////////////////////////////////////////
+
+(function($){
+	var form_data;
+
+  $.fn.VenueDriverFormWidget = function(options) {
+      var defaults = {
+      		'form_type'  : 'GUESTLIST',		//GUESTLIST or RESERVATION
+          'api_token'  : '',						//Account API key
+          'venue_split': true,					//whether or not to display a separate drop-down for venues and events.
+          'cache'      : false,					//cache response
+          'cacheTTL'   : 10080,					//how long to keep cache (7 days)
+          'account_id' : 0,							//venuedriver account id
+          'venues'     : []							//venues to load data for
+      };
+			var settings = $.extend(true, {}, defaults, options);
+
+      return this.each(function() {
+      	// Draw the form and a loading indicator so that users see something during the API call.
+        $(this).append("<div id='loading'><img src='http://widgets.venuedriver.com/images/ajax-loader.gif'/></div>");
+      });
+  };
+
+	var fetch_data = function() {
+		//load event data
+	}
+
+	var build_form = function() {
+		//assemble form HTML
+	}
+})(jQuery);
