0fe5297e76f87f7a455fa7a8808e22e722b23c31
Rich Steinschneider <rcsteins@gmail.com>
2012-05-25 17:27:42 -0400

    load json data from localhost:3000

--- public/js/CalendarEventsWidget.js
+++ public/js/CalendarEventsWidget.js
@@ -1,4 +1,8 @@
 var VenueDriverCalendarEventsWidget;
+var test_url = 'http://localhost:3000/api/accounts/1/events/calendar_month?month=5&year=2012&token=test'
+var test_json;
 var test = function(){
-  console.log("it works");
+  $.getJSON(test_url,function(data){
+    test_json = data;
+  })
 }
