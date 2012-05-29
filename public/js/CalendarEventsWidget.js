5578f01aea1c155ac411c2b0b94838bc03c8c229
Rich Steinschneider <rcsteins@gmail.com>
2012-05-29 19:27:11 -0400

    construct beginning of calendar scaffolding

--- public/js/CalendarEventsWidget.js
+++ public/js/CalendarEventsWidget.js
@@ -1,21 +1,27 @@
 var VenueDriverCalendarEventsWidget;
 // var test_url = 'http://localhost:3000/api/accounts/1/events/calendar_month?month=5&year=2012&token=test'
-// **calendar options**
+// **Calendar Options**
 // TODO:day of the first collumn, as in "calendar starts with sunday/monday"
 // api_type - "account" or "venue"
 // api_id - id of the api resource
+// div_id - the div where we insert the calendar
 var glb_debug = true;
 VenueDriverCalendarEventsWidget = function(options){
   this.test_http = 'http://localhost:3000/api/';
   this.real_http = 'http://www.venuedriver.com/api/';
+  this.div_id = '#' + options.div_id
   this.api_type = options.api_type;
   this.api_id = options.api_id;
   this.date = Date.today(); //calendar defaults to current month 
   this.json_events ={};
-  this.setMonth = function(year,month) { //month param counts from 1
+  this.setMonth = function(year,month) { //wrapper so that month param counts from 1
     this.date = new Date(year,month -1);
   };
-  this.month = function(){
+  this.changeMonth = function(year,month){ //change month and pull events
+    this.setMonth(year,month);
+    this.pull_api_events();
+  };
+  this.month = function(){ //wrapper so that month counts from one
     return this.date.getMonth()+1;
   };
   this.year = function(){
@@ -30,14 +36,30 @@ VenueDriverCalendarEventsWidget = function(options){
     $.getJSON(url,function(data){
       calendar.json_events = data;
       if (glb_debug){console.log(calendar.json_events.length)}
+      calendar.events_ready();
     });
   };
+  this.construct_scaffolding = function(){
+    var number_of_days = this.date.getDaysInMonth();
+    //create container div
+    $('#cal-test').html("<div id='calendar-container'>");
+    $('</div>').insertAfter("#calendar-container");
+    
+    
+    
+    $('#calendar-container').prepend("Im inside this div");
+  };
+  this.events_ready = function() {
+    this.construct_scaffolding();
+  };
+  this.pull_api_events(); 
 }
 
-var test_obj = new VenueDriverCalendarEventsWidget({api_type:"account",api_id:1});
-var test_obj2 = new VenueDriverCalendarEventsWidget({api_type:"venue",api_id:1});
 var mini_test = function(cal) {
-  cal.setMonth(2012,5);
-  cal.pull_api_events(); 
+  cal.changeMonth(2012,5);
 }
-mini_test(test_obj);mini_test(test_obj2);
+
+$(document).ready(function() {
+  var test_obj = new VenueDriverCalendarEventsWidget({api_type:"account",api_id:1,div_id:'cal-test'});
+});
+
