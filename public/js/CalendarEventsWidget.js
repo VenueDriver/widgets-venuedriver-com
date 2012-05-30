5002c84a355931cc6f30d04105e7be6e0015c5ec
Rich Steinschneider <rcsteins@gmail.com>
2012-05-30 00:12:39 -0400

    format table header so that correct days show up

--- public/js/CalendarEventsWidget.js
+++ public/js/CalendarEventsWidget.js
@@ -7,6 +7,33 @@ var VenueDriverCalendarEventsWidget;
 // first_day -the first day that shows
 // javascript represents sunday as 0, monday as 1...saturday as 6
 var glb_debug = true;
+var Utils = {
+  day_string_to_number: function(day_str) { //defaults to sunday if day_str is mispelled
+    switch(day_str){
+      case "Sunday": return 0;
+      case "Monday": return 1;
+      case "Tuesday": return 2;
+      case "Wednesday": return 3;
+      case "Thursday": return 4;
+      case "Friday": return 5;
+      case "Saturday": return 6;
+      default: return 0;
+    }
+  },
+  day_number_to_string: function(day_num){
+    switch(day_num){
+      case 0: return "Sunday";
+      case 1: return "Monday";
+      case 2: return "Tuesday";
+      case 3: return "Wednesday";
+      case 4: return "Thursday";
+      case 5: return "Friday";
+      case 6: return "Saturday";
+      default: "error";
+    }
+  }
+}
+
 VenueDriverCalendarEventsWidget = function(options){
   this.test_http = 'http://localhost:3000/api/';
   this.real_http = 'http://www.venuedriver.com/api/';
@@ -15,19 +42,7 @@ VenueDriverCalendarEventsWidget = function(options){
   this.api_id = options.api_id;
   this.date = Date.today(); //calendar defaults to current month 
   this.json_events ={};
-  this.parse_first_day = function(day_str) { //defaults to sunday if day_str is mispelled
-    switch(day_str){
-      case "sunday": return 0;
-      case "monday": return 1;
-      case "tueday": return 2;
-      case "wednesday": return 3;
-      case "thursday": return 4;
-      case "friday": return 5;
-      case "saturday": return 6;
-      default: return 0
-    }
-  }
-  this.first_day = this.parse_first_day(options.first_day);
+  this.first_day = Utils.day_string_to_number(options.first_day);
   this.setMonth = function(year,month) { //wrapper so that month param counts from 1
     this.date = new Date(year,month -1);
   };
@@ -49,19 +64,30 @@ VenueDriverCalendarEventsWidget = function(options){
     var calendar =this;
     $.getJSON(url,function(data){
       calendar.json_events = data;
-      if (glb_debug){console.log(calendar.json_events.length)}
+      if (glb_debug){console.log(calendar.json_events.length);}
       calendar.events_ready();
     });
   };
+  this.prepare_table_header = function() {
+    for(i=0;i<=7;i++){
+      var day_num = this.first_day + i;
+      if(day_num >= 7) day_num-=7;
+      //I use css classes as identifiers here
+      var location = '#calendar-container .header-'+ (i+1);
+      $(location).text(Utils.day_number_to_string(day_num));
+    }
+  }
   this.construct_scaffolding = function(){
     var number_of_days = this.date.getDaysInMonth();
     //create container div
     $(this.div_id).html("<div id='calendar-container'>");
     $('</div>').insertAfter("#calendar-container");
     
+    table_template = $('.clone-me').clone().attr('class','cal-table').attr('style','')
+    $('#calendar-container').append(table_template);
+    this.prepare_table_header();
     
-    
-    $('#calendar-container').prepend("Im inside this div");
+    //$('#calendar-container').prepend("Im inside this div");
   };
   this.events_ready = function() {
     this.construct_scaffolding();
@@ -74,6 +100,6 @@ var mini_test = function(cal) {
 }
 
 $(document).ready(function() {
-  var test_obj = new VenueDriverCalendarEventsWidget({api_type:"account",api_id:1,div_id:'cal-test',first_day:'monday'});
+  var test_obj = new VenueDriverCalendarEventsWidget({api_type:"account",api_id:1,div_id:'cal-test',first_day:'Monday'});
 });
 
