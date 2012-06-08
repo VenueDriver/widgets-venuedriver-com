7f812046bd5a1022edb5604611c63d447ddc366f
Rich Steinschneider <rcsteins@gmail.com>
2012-06-08 14:10:12 -0400

    add new spec file for Calendar Widget

---
+++ spec/javascripts/CalendarWidgetSpec.js
@@ -0,0 +1,15 @@
+describe("Calendar Widget", function() {
+  
+  function setup(){
+    //setup
+  }
+  
+  function clean(){
+
+  }
+  
+  it("should not create errors", function(){
+
+  });
+  
+});
