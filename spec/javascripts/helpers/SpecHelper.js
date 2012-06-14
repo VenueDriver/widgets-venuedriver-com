5d0cae63c80b3ab0056edbed559b1a5e57fac22f
Rich Steinschneider <rcsteins@gmail.com>
2012-06-14 14:52:44 -0400

    add a few options to mocked events

--- spec/javascripts/helpers/SpecHelper.js
+++ spec/javascripts/helpers/SpecHelper.js
@@ -11,7 +11,7 @@ function make_event_json(options){
     cover: null,
     created_at: "2012/01/02 14:59:58 -0400",
     date: options.date,    //ex 2012/06/01,
-    description: "Factory Girl generated event 0",
+    description: options.description || "No Deescription Given",
     event_id: options.id.to_string, //must
     friendly_id: null,
     guestlist_URL: null,
@@ -35,7 +35,7 @@ function make_event_json(options){
     tickets_URL: null,
     tickets_sold_count: 0,
     time: null,
-    title: options.title,
+    title: options.description || "No Title given",
     updated_at: "2012/06/13 14:59:58 -0400",
     venue_id: "1",
     wiki_name: null
