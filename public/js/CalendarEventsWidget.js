c3ac1358d859adad5159f68d959aa9644d573889
Rich Steinschneider <rcsteins@gmail.com>
2012-06-14 16:25:16 -0400

    remove placeholder strings from table template

--- public/js/CalendarEventsWidget.js
+++ public/js/CalendarEventsWidget.js
@@ -307,6 +307,6 @@ VenueDriverCalendarEventsWidget = function(options){
 }
 
 $(document).ready(function() {
-  //window.t = new VenueDriverCalendarEventsWidget({api_type:"account",api_id:1,div_id:'cal-test',first_day:'Monday',testing:true,day_bottom:false});
+  window.t = new VenueDriverCalendarEventsWidget({api_type:"account",api_id:1,div_id:'cal-test',first_day:'Monday',testing:true,day_bottom:false});
 });
 
