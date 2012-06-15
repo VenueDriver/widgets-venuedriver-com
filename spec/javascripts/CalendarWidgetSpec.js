3f4e45cf89bd6f273afe8a3561ee0d220dbee1fa
Rich Steinschneider <rcsteins@gmail.com>
2012-06-15 15:29:17 -0400

    refactor for further development

--- spec/javascripts/CalendarWidgetSpec.js
+++ spec/javascripts/CalendarWidgetSpec.js
@@ -12,31 +12,31 @@ describe("Calendar Widget", function() {
       mock_date_today('2012/06/01')
     });
     
-    it("Can be Monday", function(){
+    it("can be Monday", function(){
       test_first_day('Monday');
     });
     
-    it("Can be Tuesday", function(){
+    it("can be Tuesday", function(){
       test_first_day('Tuesday');
     });
     
-    it("Can be Wednesday", function(){
+    it("can be Wednesday", function(){
       test_first_day('Wednesday');
     });
     
-    it("Can be Thursday", function(){
+    it("can be Thursday", function(){
       test_first_day('Thursday');
     });
     
-    it("Can be Friday", function(){
+    it("can be Friday", function(){
       test_first_day('Friday');
     });
     
-    it("Can be Saturday", function(){
+    it("can be Saturday", function(){
       test_first_day('Saturday');
     });
     
-    it("Can be Sunday", function(){
+    it("can be Sunday", function(){
       test_first_day('Sunday');
     });
     
@@ -48,18 +48,24 @@ describe("Calendar Widget", function() {
      
   });
   
-  it("shows events in the correct position in the calendar",function(){
-    var options = {api_type:"account",api_id:1,div_id:'cal-test',first_day:"Monday"}
-    mock_date_today('2012/06/01');
-    json = [];
-    json.push(make_event_json({event_title:'Event 1',event_date:'2012/06/04',id:1}));
-    json.push(make_event_json({event_title:'Event 2',event_date:'2012/06/05',id:2}));
-    json.push(make_event_json({event_title:'Event 3',event_date:'2012/06/06',id:3}));
-    jQuery.ajaxMock.url('http://www.venuedriver.com/api/accounts/1/events/calendar_month?month=6&year=2012&token=test', json);
-    window.cal = new VenueDriverCalendarEventsWidget(options);
-    expect($('#calendar-container #2012-06-04 .event-content-area :first-child a').text()).toEqual("Event 1");
-    expect($('#calendar-container #2012-06-05 .event-content-area :first-child a').text()).toEqual("Event 2");
-    expect($('#calendar-container #2012-06-06 .event-content-area :first-child a').text()).toEqual("Event 3");
+  describe("Displaying Events",function(){
+    
+    beforeEach(function() {
+      var options = {api_type:"account",api_id:1,div_id:'cal-test',first_day:"Monday"}
+      mock_date_today('2012/06/01');
+      json = [];
+      json.push(make_event_json({event_title:'Event 1',event_date:'2012/06/04',id:1}));
+      json.push(make_event_json({event_title:'Event 2',event_date:'2012/06/05',id:2}));
+      json.push(make_event_json({event_title:'Event 3',event_date:'2012/06/06',id:3}));
+      jQuery.ajaxMock.url('http://www.venuedriver.com/api/accounts/1/events/calendar_month?month=6&year=2012&token=test', json);
+      window.cal = new VenueDriverCalendarEventsWidget(options);
+    });
+    
+    it('shows events in their proper positions', function(){
+      expect($('#calendar-container #2012-06-04 .event-content-area :first-child a').text()).toEqual("Event 1");
+      expect($('#calendar-container #2012-06-05 .event-content-area :first-child a').text()).toEqual("Event 2");
+      expect($('#calendar-container #2012-06-06 .event-content-area :first-child a').text()).toEqual("Event 3");
+    });
   });
   
 });
